5d548331cbd4dd6674d49b997f2ef3ebc3092dbf
Gabriel-Cherem <gabrieljeremiasc64@gmail.com>
2026-08-16 13:00:37 -0300

    refactor: Separar templates home em index (base) e content

--- projeto/views.py
+++ projeto/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index_view(request):
-    return render(request, 'home/index.html')
+    return render(request, 'home/content.html')
